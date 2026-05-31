2866e6fdb863792ad502840974d247570849be79
Pallavi Srinivas <pallavisrinivas7647@gmail.com>
2026-05-31 14:54:43 +0530

    Added rectangle drawing functionality

--- main.c
+++ main.c
@@ -1,10 +1,44 @@
-
 #include <stdio.h>
 
-int main() {
+#define ROWS 20
+#define COLS 50
 
-    printf("2D Graphics Editor Project\n");
+char canvas[ROWS][COLS];
+
+void initializeCanvas() {
+    int i, j;
+    for(i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void drawRectangle(int x, int y, int width, int height) {
+    int i, j;
+    for(i = y; i < y + height; i++) {
+        for(j = x; j < x + width; j++) {
+            canvas[i][j] = '*';
+        }
+    }
+}
+
+void displayCanvas() {
+    int i, j;
+    for(i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    initializeCanvas();
+
+    drawRectangle(5, 3, 10, 5);
+
+    displayCanvas();
 
     return 0;
 }
-
