8cfb343727cd011ef5189136083fc37e4344f758
Pallavi Srinivas <pallavisrinivas7647@gmail.com>
2026-05-31 15:23:11 +0530

    Added line triangle circle delete and modify features

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 #define ROWS 20
 #define COLS 50
@@ -22,6 +23,47 @@ void drawRectangle(int x, int y, int width, int height) {
         }
     }
 }
+void drawLine(int x1, int x2, int y) {
+    int i;
+    for(i = x1; i <= x2; i++) {
+        canvas[y][i] = '*';
+    }
+}
+void drawTriangle(int x, int y, int height) {
+    int i, j;
+
+    for(i = 0; i < height; i++) {
+        for(j = 0; j <= i; j++) {
+            canvas[y + i][x + j] = '*';
+        }
+    }
+}
+
+void drawCircle(int centerX, int centerY, int radius) {
+    int x, y;
+
+    for(y = 0; y < ROWS; y++) {
+        for(x = 0; x < COLS; x++) {
+            int dx = x - centerX;
+            int dy = y - centerY;
+
+            if(dx * dx + dy * dy <= radius * radius) {
+                canvas[y][x] = '*';
+            }
+        }
+    }
+}
+
+void clearCanvas() {
+    int i, j;
+
+    for(i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
 
 void displayCanvas() {
     int i, j;
@@ -32,13 +74,31 @@ void displayCanvas() {
         printf("\n");
     }
 }
+int main(){
+int choice;
 
-int main() {
-    initializeCanvas();
+initializeCanvas();
 
+printf("1. Draw Shapes\n");
+printf("2. Delete All Shapes\n");
+printf("3. Modify Rectangle\n");
+printf("Enter choice: ");
+scanf("%d", &choice);
+
+if(choice == 1) {
     drawRectangle(5, 3, 10, 5);
+    drawLine(20, 35, 10);
+    drawTriangle(30, 2, 5);
+    drawCircle(40, 15, 3);
+}
+else if(choice == 2) {
+    clearCanvas();
+}
+else if(choice == 3) {
+    drawRectangle(5, 3, 15, 8);
+}
 
-    displayCanvas();
+displayCanvas();
 
-    return 0;
+return 0;
 }
